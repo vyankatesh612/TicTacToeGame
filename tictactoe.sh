ce769b1cb44dc6c15143a25a0a2b349418e9337f
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-14 11:13:48 +0530

    ADDED : assign letter to player

--- tictactoe.sh
+++ tictactoe.sh
@@ -11,3 +11,17 @@ function resetGameboard()
 		done
 	}
 resetGameboard
+
+function assignletter()
+	{
+		local letter1="X"
+		local letter2="O"
+		local random=$((RANDOM%2))
+		if [ $random == 0 ]
+		then 
+			player=$letter1
+		else
+			player=$letter2
+		fi
+	}
+assignletter
